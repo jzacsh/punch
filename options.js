69e6f05a0252ab1bc54b2fa2e0490de83b6cf6a9
Jonathan Zacsh <jzacsh@gmail.com>
2011-07-20 20:34:59 -0400

    making sure my old attempts from nodejsbook are not in the plan.

--- options.js
+++
@@ -1,7 +0,0 @@
-var options = {
-    server: {
-        port: 8888,
-    }
-};
-
-exports.options = options;
--- punchcard.js
+++
@@ -1,7 +0,0 @@
-var server = require('./server.js');
-var respond = require('./router.js').respond; //@TODO: ref this file in ./router/
-var punch = require('./punch.js');
-var options = require('./options.js').options;
-
-server.start(respond, options);
-console.log('Server initiated with config: %s', options);
