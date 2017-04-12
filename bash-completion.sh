609c18f390a18b93b5ea65d9221d4a7842900ac1
Jonathan Zacsh <jzacsh@gmail.com>
2017-04-11 22:06:01 -0400

    bug fix: bash-completion should check for possible CLIs
    NOTE: works perfectly, except unfortunately  triggers for `punch q` without its
    requisite 2nd argument.

--- bash-completion.sh
+++ bash-completion.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 __punchClientCompletion() {
-  [ "${COMP_WORDS[COMP_CWORD-1]}" = -c ] || return 0 # only autocomplete clients
+  {
+    local firstArg; firstArg="${COMP_WORDS[COMP_CWORD-2]}"
+    local secondArg; secondArg="${COMP_WORDS[COMP_CWORD-1]}"
+    [[ "$firstArg" = punch ]] || [[ "$firstArg" = p ]] ||
+    [[ "$firstArg" = bill ]] || {
+      { [[ "$firstArg" = q ]] || [[ "$firstArg" = query ]]; } && {
+        [[ "$secondArg" = report ]] ||
+        [[ "$secondArg" = bills ]] ||
+        [[ "$secondArg" = bill ]];
+      };
+    }
+  } || return 0 # only autocomplete clients
 
   local clients; clients="$(punch query list)"
   COMPREPLY=( $(compgen -W "${clients[@]}" -- "${COMP_WORDS[$COMP_CWORD]}") )
