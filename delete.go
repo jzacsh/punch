b8c395ffe66f44bca4688357c45a4dd11ec97ce2
Jonathan Zacsh <jzacsh@gmail.com>
2017-04-13 13:50:49 -0400

    feature: safe~ish deletions

--- delete.go
+++ delete.go
@@ -27,7 +27,7 @@ func (d *DeleteCmd) isTargetingBill() bool {
 
 func (d *DeleteCmd) String() string {
 	return fmt.Sprintf(
-		"Delete '%s'-%s at %s (timestamp %d) [dry-run=%t]",
+		"Delete '%s'-%s at %s [@%d] [dry-run=%t]",
 		d.Client,
 		d.Target,
 		d.At.Format(format_dateTime),
@@ -35,8 +35,14 @@ func (d *DeleteCmd) String() string {
 		d.IsDryRun)
 }
 
-func (d *DeleteCmd) Report(db *sql.DB) error {
+// `punchOut` will be -1 when d.Target is 'bill', else:
+// - punchOut of -1 indicates `d` is a request to delete a punch-out, meaning
+//   there's no corresponding punch-out because `d` itself represents a punch-out.
+// - punchOut of > -1 indicates `d` is a punch-in, and punchOut is the timestamp
+//   of `d`'s corresponding punch-out record.
+func (d *DeleteCmd) Report(db *sql.DB) (punchOut int64, _ error) {
 	fmt.Printf("%s...\n", d)
+	punchOut = -1
 
 	if d.isTargetingBill() {
 		rows, e := db.Query(`
@@ -45,18 +51,18 @@ func (d *DeleteCmd) Report(db *sql.DB) error {
 		AND startclusive IS ?
 		;`, d.Client, d.At.Unix())
 		if e != nil {
-			return fmt.Errorf("querying DB: %s", e)
+			return punchOut, fmt.Errorf("querying DB: %s", e)
 		}
 		defer rows.Close()
 		foundTarget := false
 		for rows.Next() {
 			b, e := scanToBill(rows)
 			if e != nil {
-				return fmt.Errorf("parsing DB response: %s", e)
+				return punchOut, fmt.Errorf("parsing DB response: %s", e)
 			}
 
 			if foundTarget {
-				return fmt.Errorf("malformed data: found TWO payperiods sharing start time")
+				return punchOut, fmt.Errorf("malformed data: found TWO payperiods sharing start time")
 			}
 
 			foundTarget = true
@@ -66,15 +72,115 @@ func (d *DeleteCmd) Report(db *sql.DB) error {
 				b.String(false /*showTimezone*/))
 		}
 		if !foundTarget {
-			return fmt.Errorf(
+			return punchOut, fmt.Errorf(
 				"no '%s' payperiods start at %s",
 				d.Client, d.At.Format(format_dateTime))
 		}
 	} else {
-		return fmt.Errorf("reporting for punch-deletions, not yet implemented") // TODO
+		rows, e := db.Query(`
+		SELECT * FROM punchcard
+		WHERE project IS ?
+		AND punch >= ?
+		ORDER BY punch ASC
+		LIMIT 2
+		;`, d.Client, d.At.Unix())
+		if e != nil {
+			return punchOut, fmt.Errorf("querying DB: %s", e)
+		}
+		defer rows.Close()
+
+		i := 0
+		var match, second *CardSchema
+		for rows.Next() {
+			c, e := scanToCard(rows)
+			if e != nil {
+				return punchOut, fmt.Errorf("parsing DB response: %s", e)
+			}
+
+			if i == 0 {
+				if c.Punch != d.At {
+					return punchOut, fmt.Errorf("no '%s' punch at %s",
+						d.Client, d.At.Format(format_dateTime))
+				}
+				match = c
+			} else {
+				second = c
+			}
+
+			i++
+		}
+		if match == nil {
+			return punchOut, fmt.Errorf(
+				"No '%s' punches found between %s and now",
+				d.Client,
+				d.At.Format(format_dateTime))
+		}
+
+		isSessionDeletion := match.IsStart // Deleting an entire session
+		if isSessionDeletion {
+			if second.IsStart {
+				return punchOut, fmt.Errorf(
+					"malformed db: found TWO punch-ins in a row, second at %d",
+					second.Punch.Unix())
+			}
+			if second == nil {
+				fmt.Printf(
+					"Effectively deletes an active %s-session that started %s ago\n\tnote: '%s'\n",
+					d.Client,
+					time.Now().Sub(d.At),
+					fromNote(match.Note))
+			} else {
+				punchOut = second.Punch.Unix()
+
+				session := fmt.Sprintf(
+					"\tstart note: '%s'\n\tend   note: '%s'",
+					fromNote(match.Note), fromNote(second.Note))
+				fmt.Printf(
+					"Effectively deletes entire %s-session that ended %s [@%d]:\n%s\n",
+					second.Punch.Sub(d.At),
+					second.Punch.Format(format_dateTime),
+					punchOut,
+					session)
+			}
+		} else {
+			// We're effectively punching back in (ie: we punched out out of a
+			// work-session, and want to undo that, indicating we've still been
+			// working until now, this whole time).
+
+			rows, e := db.Query(`
+			SELECT COUNT(DISTINCT punch)
+			FROM punchcard
+			WHERE project IS ?
+			AND punch > ?
+			;`, d.Client, d.At.Unix())
+			if e != nil {
+				return punchOut, fmt.Errorf("querying DB: %s", e)
+			}
+			defer rows.Close()
+
+			count := -1
+			for rows.Next() {
+				if e := rows.Scan(&count); e != nil {
+					return punchOut, fmt.Errorf("querying DB: %s", e)
+				}
+			}
+			if count == -1 {
+				panic("somehow pointer not populated by successful rows.Scan()")
+			}
+
+			if count != 0 {
+				return punchOut, fmt.Errorf(
+					"Re-opening work session with new sessions opened since will cause data inconsistency (%d punches found since). HINT: to delete an ENTIRE session, delete its punch-IN time.", count)
+			}
+
+			fmt.Printf(
+				"Effectively re-opening session that ended %s ago at %s\n",
+				time.Now().Sub(d.At),
+				d.At.Format(format_dateTime))
+		}
 	}
 
-	return nil
+	return punchOut, nil
 }
 
 func parseDeleteCmd(args []string) (*DeleteCmd, error) {
@@ -126,7 +232,8 @@ func subCmdDelete(dbPath string, args []string) error {
 	}
 	defer db.Close()
 
-	if e := cmd.Report(db); e != nil {
+	punchOut, e := cmd.Report(db)
+	if e != nil {
 		return e
 	}
 
@@ -143,7 +250,25 @@ func subCmdDelete(dbPath string, args []string) error {
 			return fmt.Errorf("preparing SQL for deletion: %s", e)
 		}
 	} else {
-		return fmt.Errorf("%s deletion not yet implemented :(", cmd.Target) // TODO
+		if punchOut == -1 { // cmd.At is a punch-out, we want to re-open the session
+			stmt, e = db.Prepare(`
+			DELETE FROM punchcard
+			WHERE project iS ?
+			AND punch IS ?
+			;`)
+			if e != nil {
+				return fmt.Errorf("preparing SQL for deletion: %s", e)
+			}
+		} else { // cmd.At is a punch-in, we want to delete the whole session
+			stmt, e = db.Prepare(`
+			DELETE FROM punchcard
+			WHERE project iS ?
+			AND punch IN (?, ?)
+			;`)
+			if e != nil {
+				return fmt.Errorf("preparing SQL for deletion: %s", e)
+			}
+		}
 	}
 
 	if cmd.IsDryRun {
@@ -156,9 +281,15 @@ func subCmdDelete(dbPath string, args []string) error {
 			return e
 		}
 	} else {
-		return fmt.Errorf(
-			"%s deletion exec not yet implemented :(",
-			cmd.Target) // TODO
+		if punchOut == -1 {
+			if _, e := stmt.Exec(cmd.Client, cmd.At.Unix()); e != nil {
+				return e
+			}
+		} else {
+			if _, e := stmt.Exec(cmd.Client, cmd.At.Unix(), punchOut); e != nil {
+				return e
+			}
+		}
 	}
 
 	fmt.Println("Done.")
