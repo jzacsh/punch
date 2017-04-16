b9a0ff4c9264b9504e618527b2a93bae88a88c36
Jonathan Zacsh <jzacsh@gmail.com>
2017-04-16 02:27:22 -0400

    noop(refactor) hide muddy cli parsing

--- amend.go
+++ amend.go
@@ -9,33 +9,45 @@ import (
 	"time"
 )
 
+// TARGET_STAMP, [NOTE], error
+func parseAmendCli(args []string) (time.Time, string, error) {
+	var target time.Time
+	if len(args) < 1 {
+		return target, "", fmt.Errorf("argument TARGET_STAMP is required")
+	}
+
+	targetStamp, e := strconv.ParseInt(strings.TrimSpace(args[0]), 10, 64)
+	if e != nil {
+		return target, "", fmt.Errorf("parsing unix timestamp, TARGET_STAMP ('%s'), %s", args[0], e)
+	}
+
+	target = time.Unix(targetStamp, 0 /*nanoseconds*/)
+
+	var replacement string
+	if len(args) > 1 {
+		replacement = strings.TrimSpace(strings.Join(args[1:], " "))
+	}
+
+	return target, replacement, nil
+}
+
 func subCmdAmend(dbPath string, args []string) error {
+	target, note, e := parseAmendCli(args)
+	if e != nil {
+		return e
+	}
+	isDeletion := len(note) < 1
+
 	db, e := sql.Open("sqlite3", dbPath)
 	if e != nil {
 		return fmt.Errorf("punch cards: %s", e)
 	}
 	defer db.Close()
 
-	if len(args) < 1 {
-		return fmt.Errorf("argument TARGET_STAMP is required")
-	}
-
-	targetStamp, e := strconv.ParseInt(strings.TrimSpace(args[0]), 10, 64)
-	if e != nil {
-		return fmt.Errorf("parsing unix timestamp, TARGET_STAMP ('%s'), %s", args[0], e)
-	}
-
-	target := time.Unix(targetStamp, 0 /*nanoseconds*/)
-
-	var replacement string
-	if len(args) > 1 {
-		replacement = strings.TrimSpace(strings.Join(args[1:], " "))
-	}
-	isDeletion := len(replacement) < 1
 	return fmt.Errorf(
 		"NOTE amendment not yet implemented, but got:\n\t Note: '%s'\n\tis deletion: %t\n\twhich: %s (@%s)\n",
-		replacement,
+		note,
 		isDeletion,
 		target,
-		targetStamp)
+		target.Unix())
 }
